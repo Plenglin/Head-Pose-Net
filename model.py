7af2ae329015d2a75066ba29805bd29ad2d42f20
Astrid Augusta Yu <astridyu3.14@gmail.com>
2019-05-29 15:11:40 -0700

    Session-based training

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import util
 
 def create_model():
     inputs = keras.Input(shape=(util.FEATURES,))
-    hidden_1 = keras.layers.Dense(20, activation='relu', kernel_regularizer='l2')(inputs)
+    hidden_1 = keras.layers.Dense(30, activation='relu', kernel_regularizer='l2')(inputs)
     hidden_2 = keras.layers.Dense(10, activation='relu', kernel_regularizer='l2')(hidden_1)
     predictions = keras.layers.Dense(2, activation='linear')(hidden_2)
 
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
-
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -10,25 +10,30 @@ import model
 
 EPOCHS = 1000
 
-inputs = pd.read_csv('ml_inputs.csv', index_col=0)
-outputs = pd.read_csv('ml_outputs.csv', index_col=0)
+inputs = pd.read_csv('ml_inputs.csv', index_col=0).astype(np.float32)
+outputs = pd.read_csv('ml_outputs.csv', index_col=0).astype(np.float32)
 X_train, X_test, y_train, y_test = train_test_split(inputs, outputs, shuffle=True)
 
-model = model.create_model()
 
-early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=25)
-model.fit(
-    X_train, y_train,
-    epochs=EPOCHS, 
-    validation_split=0.2,
-    batch_size=32, 
-    callbacks=[early_stop])
+with tf.Session() as sess:
+    tf.keras.backend.set_session(sess)
+    model = model.create_model()
+    loss = tf.losses.mean_squared_error(labels=y_train, predictions=model(X_train))
+    optimizer = tf.train.AdamOptimizer(0.01)
+    train = optimizer.minimize(loss)
 
-loss, mae, mse = model.evaluate(X_test, y_test)
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=25)
+    model.fit(
+        X_train, y_train,
+        epochs=EPOCHS, 
+        validation_split=0.2,
+        batch_size=32, 
+        callbacks=[early_stop])
+    loss, mae, mse = model.evaluate(X_test, y_test)
 
-print("Testing set Mean Abs Error: {:5.2f}".format(mae))
+    print("Testing set Mean Abs Error: {:5.2f}".format(mae))
 
-y_test_hat = model.predict(X_test)
-residuals = y_test_hat - y_test
-plt.hist(residuals['Yaw'], bins = 25)
-plt.show()
+    y_test_hat = model.predict(X_test)
+    residuals = y_test_hat - y_test
+    plt.hist(residuals['Yaw'], bins = 25)
+    plt.show()
